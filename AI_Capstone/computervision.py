6542a83b7490c6701d9f2967934ffcb1b2e4462a
Gouled Awad <69343228+gawad21@users.noreply.github.com>
2023-03-16 16:17:28 -0400

    Add files via upload

--- AI_Capstone/computervision.py
+++ AI_Capstone/computervision.py
@@ -7,51 +7,12 @@ import time as t
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, etc.
 model = torch.hub.load('ultralytics/yolov5', 'custom','bestv2.pt',force_reload=True,trust_repo=True)  # custom trained model
-#vid = cv2.VideoCapture(0)
-im = 'C:\\Users\\goule\\OneDrive\\Desktop\\YOLO\\leaf.jpg'
-'''
+
 def Model_detection():
+    vid = cv2.VideoCapture(1)
     Class_detected = [] 
     Confidence_detected = []  
-    vid = cv2.VideoCapture(0)
-    # define a video capture object
-    
-
-    while(True):
-      
-    # Capture the video frame
-    # by frame
-        #ret, frame = vid.read()
-        results =  model(im)
-        results.show()
-        results.xyxy[0]
-        df=results.pandas().xyxy[0]
-
-        Class_detected = df['class'].tolist()
-        Confidence_detected = df['confidence'].tolist()
-    # Display the resulting frame
-        #cv2.imshow('frame', frame)
-        if  df.empty:
-            print("The DataFrame is empty. Breaking out of the loop.")
-            break
-        Class_detected = df['class'].tolist()
-        Confidence_detected = df['confidence'].tolist()
-        break
-
-    return Class_detected, Confidence_detected
-
-'''
-# After the loop release the cap object
-#vid.release()
-# Destroy all the windows
-#v2.destroyAllWindows()
-#vid = cv2.VideoCapture("C:\\Users\\goule\\OneDrive\\Documents\\Courses\\Winter 2023\\Capstone2\\Test\\test.mp4")
-path="C:\\Users\\goule\\OneDrive\\Documents\\Courses\\Winter 2023\\Capstone2\\Test\\test.mp4"
-def Model_detection():
-    vid = cv2.VideoCapture(path)
-    Class_detected = [] 
-    Confidence_detected = []  
-    vid = cv2.VideoCapture(0)
+    #vid = cv2.VideoCapture(0)
     # define a video capture object
     
 
@@ -60,7 +21,12 @@ def Model_detection():
     # by frame
     ret, frame = vid.read()
     results =  model(frame)
-    #results.show()
+   
+    im = 'Sam.jpg'  # or file, Path, URL, PIL, OpenCV, numpy, list
+
+# Inference
+    # results = model(im)
+    results.show()
     results.xyxy[0]
     df=results.pandas().xyxy[0]
     Class_detected = df['class'].tolist()
@@ -71,3 +37,4 @@ def Model_detection():
     Confidence_detected = df['confidence'].tolist()
   
     return Class_detected, Confidence_detected
+
--- AI_Capstone/test_database.py
+++ AI_Capstone/test_database.py
@@ -6,14 +6,14 @@ cred = credentials.Certificate('aquaspecta-creds.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def update_status_flags(inDistress, isDrowning, maxedLeaves):
+def update_status_flags(inDistress, isDrowning, LeafPercent):
     # Reference to document containing status flags
     statRef = db.collection(u'user1').document(u'status-flags')
 
     # For updating the three status flags
     statRef.update({u'inDistress': inDistress})
     statRef.update({u'isDrowning': isDrowning})
-    statRef.update({u'maxedLeaves': maxedLeaves})
+    statRef.update({u'LeafPercent':LeafPercent})
 
     # Reference to document containing user settings
     doc_ref = db.collection(u'user1').document(u'settings')
@@ -25,3 +25,18 @@ def update_status_flags(inDistress, isDrowning, maxedLeaves):
         print(u'No such document!')
 
 
+def update_status_flags_alarm(alarmON):
+    # Reference to document containing status flags
+    statRef = db.collection(u'user1').document(u'status-flags')
+
+    # For updating the three status flags
+    statRef.update({u'alarmON': alarmON})
+
+    # Reference to document containing user settings
+    doc_ref = db.collection(u'user1').document(u'settings')
+
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        print(u'No such document!')
